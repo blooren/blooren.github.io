72856d10d9dd2660399cfafbb064c824403dd863
blooren <blooren@outlook.com>
2022-11-08 10:23:39 -0300

    added a condition to change content in logo

--- code.js
+++ code.js
@@ -1,21 +1,16 @@
-// logo.style.color = "blue";
 const logo = document.getElementById("blooren");
-const sticky = logo.offsetTop;
-
-function changeColor(){
-    logo.style.color = "blue", "white";
-}
-
-function verticalStatus(){
-    console.log(window.pageYOffset);
-}
-
-function myFunction() {
-    if (window.pageYOffset <= 96) {
-    logo.style.color="#a3363d"
-    logo.innerHTML = "Blooren"
-} else {
-    logo.style.color="blue";
-    logo.innerHTML = "Blooren/>"
+document.onscroll = function(){ 
+    var pos = getVerticalScrollPercentage(document.body)
+    if(pos>=80){
+        // logo.style.color="blue";
+        logo.innerHTML = "Blooren/>"
+    }else{
+        logo.style.color="#a3363d";
+        logo.innerHTML = "Blooren"
     }
   }
+
+function getVerticalScrollPercentage(elm){
+  var p = elm.parentNode;
+  return(elm.scrollTop || p.scrollTop) / (p.scrollHeight - p.clientHeight ) * 100;
+}
